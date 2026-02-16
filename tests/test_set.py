1a2559c56052a15e8a62ecd542e8e6747d28b99b
MatteoRaffaeleDeSilvestri <74906990+MatteoRaffaeleDeSilvestri@users.noreply.github.com>
2026-02-16 22:07:07 +0000

    Restructure test.py

--- tests/test_set.py
+++ tests/test_set.py
@@ -1,20 +1,18 @@
 from fastapi.testclient import TestClient
-from main import app
+from src.main import app
 
 client = TestClient(app)
 
-data = {'text': 'This product works exactly as advertised and has significantly simplified my daily routine'}
-response = client.post('/analyze', json=data)
-
-assert response.status_code == 200
-assert isinstance(response.json(), list)
-
-for item in response.json():
-    assert 'label' in item
-    assert 'score' in item
-    assert isinstance(item['label'], str)
-    assert isinstance(item['score'], float)
+def CI_test_sentiment_analysis():
     
-print(f'Sentiment analysis results for input: "{data['text']}"')
-for i in response.json():
-    print(f"- {i['label']}: {i['score']:.4f}")
+    data = {'text': 'This product works exactly as advertised and has significantly simplified my daily routine'}
+    response = client.post('/analize', json = data)
+
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+    for item in response.json():
+        assert 'label' in item
+        assert 'score' in item
+        assert isinstance(item['label'], str)
+        assert isinstance(item['score'], float)
