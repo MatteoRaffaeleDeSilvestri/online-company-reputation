e2960abd96f7930de49c2aa50fdd3e7f55f48a0b
MatteoRaffaeleDeSilvestri <74906990+MatteoRaffaeleDeSilvestri@users.noreply.github.com>
2026-02-16 21:48:14 +0000

    Bug fixing test_set.py

--- tests/test_set.py
+++ tests/test_set.py
@@ -3,36 +3,18 @@ from main import app
 
 client = TestClient(app)
 
-def test_sentiment_analysis(txt):
+data = {'text': 'This product works exactly as advertised and has significantly simplified my daily routine'}
+response = client.post('/analyze', json=data)
 
-    data = {'text': txt}
-    response = client.post('/analyze', json=data)
+assert response.status_code == 200
+assert isinstance(response.json(), list)
 
-    assert response.status_code == 200
-    assert isinstance(response.json(), list)
+for item in response.json():
+    assert 'label' in item
+    assert 'score' in item
+    assert isinstance(item['label'], str)
+    assert isinstance(item['score'], float)
     
-    for item in response.json():
-        assert 'label' in item
-        assert 'score' in item
-        assert isinstance(item['label'], str)
-        assert isinstance(item['score'], float)
-    
-    return response.json()
-
-if __name__ == '__main__':
-
-    # Positive review test
-    positive_text = 'This product works exactly as advertised and has significantly simplified my daily routine'
-    sentiment_eval = test_sentiment_analysis(positive_text)
-    
-    print(f'Sentiment analysis results for input: "{positive_text}"')
-    for i in sentiment_eval:
-        print(f"- {i['label']}: {i['score']:.4f}")
-
-    # Negative review test
-    negative_text = 'The instruction manual was incredibly confusing and lacked several crucial steps'
-    sentiment_eval = test_sentiment_analysis(negative_text)
-
-    print(f'Sentiment analysis results for input: "{negative_text}"')
-    for i in sentiment_eval:
-        print(f"- {i['label']}: {i['score']:.4f}")
+print(f'Sentiment analysis results for input: "{data['text']}"')
+for i in sentiment_eval:
+    print(f"- {i['label']}: {i['score']:.4f}")
